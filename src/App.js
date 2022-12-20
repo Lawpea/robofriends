3280b1362bcd155eee76e0ed6387c02a955bb408
Lawpea <peakelaw@hotmail.com>
2022-12-20 11:51:14 +1100

    Added scroll function and reference to JSON file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react";
 import CardList from "./CardList";
-import {robots} from './robots';
 import SearchBox from './SearchBox';
+import './App.css';
+import Scroll from './Scroll';
 
 class App extends Component 
 {
@@ -10,9 +11,16 @@ class App extends Component
     {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         }
+    }
+
+    componentDidMount()
+    {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users}))
     }
 
     onSearchChange = (event) =>
@@ -26,13 +34,22 @@ class App extends Component
             {
                 return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
             })
-        return (
-            <div className="tc">
-                <h1>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-            </div>
-        );
+        if (this.state.robots.length === 0)
+        {
+            return <h1>Loading...</h1>
+        }
+        else
+        {
+            return (
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
+                </div>
+            );
+        }
     }
 }
 
